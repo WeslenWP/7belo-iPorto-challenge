ee195e00c3b0cabdb984ded62c22b736715ad259
Weslen <weslen1905@gmail.com>
2022-05-25 18:36:42 -0300

    Instalação do Animate on Scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '7-belo-jade';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    AOS.init({
+      once: true
+    });
+  }
 }
