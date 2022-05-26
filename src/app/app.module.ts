39827d1a5278c0f58936d42b6288f2d44c2878b6
Weslen <weslen1905@gmail.com>
2022-05-25 23:51:27 -0300

    Content feito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeadlineComponent } from './modules/headline/headline.component';
+import { ContentComponent } from './modules/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadlineComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
