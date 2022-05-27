cb58a2debf80a09e0583485b9cf06d0e613ffc8d
Weslen <weslen1905@gmail.com>
2022-05-26 21:27:57 -0300

    Alteração para uso de enviromment

--- src/app/core/services/costumers.service.ts
+++ src/app/core/services/costumers.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class CostumersService {
   constructor(private _httpClient: HttpClient) { }
 
   getData(): Observable<any> {
-    return this._httpClient.get("https://gorest.co.in/public/v2/comments")
+    return this._httpClient.get(`${environment.url}/comments`)
   }
 }
