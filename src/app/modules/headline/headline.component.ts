3da5277ca99e3c41a1505fde9ee4b3ed105102e4
Weslen <weslen1905@gmail.com>
2022-05-26 21:35:08 -0300

    Limpeza

--- src/app/modules/headline/headline.component.ts
+++ src/app/modules/headline/headline.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-headline',
   templateUrl: './headline.component.html',
-  styleUrls: ['./headline.component.scss']
 })
 export class HeadlineComponent implements OnInit {
 
