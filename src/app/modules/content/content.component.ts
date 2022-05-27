db7d132529de23e10c10b957ecec2d5ab4fb49b6
Weslen <weslen1905@gmail.com>
2022-05-26 21:29:56 -0300

    Redesign e botao agora abre o modal

--- src/app/modules/content/content.component.ts
+++ src/app/modules/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormModalService } from 'src/app/core/services/form-modal.service';
 
 @Component({
   selector: 'app-content',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _modalService:FormModalService) { }
 
   ngOnInit(): void {
   }
